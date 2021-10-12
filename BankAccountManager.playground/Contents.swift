bfbf781fa1d65615806ce63416153f1f59d74ecd
Abdullah Bajaman <ambajaman@gmail.com>
2021-10-12 19:46:18 +0300

    Add: transaction struct, functions in Account Struct and Person Class. Update: functions in  Account struct

--- BankAccountManager.playground/Contents.swift
+++ BankAccountManager.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreGraphics
 
 /*
  Bank Account Manager
@@ -13,17 +14,15 @@ import Foundation
  As a user, I should be able to transfare money to other person
  
  ************ enum , extend sort,
+ vip costumer can make tracaction more than normal account
  */
 
 class Person {
     /*
-     to do:  # sort by date
      # removeAccount: try catch
      */
-    
-    
     var name : String
-    var accounts = [Account]()
+    var accounts = [Account]() // array of Account struct
     
     
     init(name: String) {
@@ -32,88 +31,134 @@ class Person {
     
     func getAllAccounts() {
         // add try catch
-        self.accounts.map{ account in
-            print("\(name) has account in \(account.name) with Amount of \(account.amount), Opening Account at \(account.date)")
+        self.accounts.map{ account in // loop to display each account in accounts array
+            print("\(name) has account in \(account.name) with Amount of \(account.amount)")
         }
         print("Total Amount is: \(getTotalAmounts())")
     }
-    func getTotalAmounts() -> Double {
-        return self.accounts.map{$0.amount}.reduce(0, {$0 + $1})
+    
+    func addAccount(newAccount: Account){ // add a new account to accounts array
+        accounts.append(newAccount)
     }
-    func removeAccount(name: String){
-        guard let deleteAccount = accounts.firstIndex(where: {$0.name == name}) else {
-            print("No \(name) Account in your Accounts ")
-            return }
-        accounts.remove(at: deleteAccount)
+    func getTotalAmounts() -> Double {//??? print total amount person has more than 1  account
+        let numAccount:Double
+        if accounts.count > 1 {
+            numAccount = accounts.map{$0.amount}.reduce(0, {$0 + $1})
+        } else { numAccount = accounts[0].amount}
+        return numAccount
     }
-    func transfare(money: Double, to: String){
-        accounts.firstIndex(where: {$0.name == to})
+    private func findIndexBy(id: Int) -> Int?{ // return index of accounts array
+        guard let id = accounts.firstIndex(where: {$0.id == id}) else {
+            print("No Account with id: \(id)")
+            return nil
+        }
+        return id
     }
+    func removeAccount(id: Int){ // remove account by id
+        guard let index = findIndexBy(id: id) else { return  }
+        accounts.remove(at: index)
+    }
+    
     
 }
 
+struct Transaction{
+    var id: Int, amount: Double, date: Date
+    init(id: Int, amount: Double, date: Date) {
+        self.id = id
+        self.amount = amount
+        self.date = Date()
+    }
+}
 
  struct Account {
      /* to do:
       @ date
       @ account type: enum personal, business, saving
-      # transfare money to other person : cash , online
+      # tranactions to other person : cash , online
       
       */
-     var name : String, amount : Double, date: Date
+     
+     var transactions = [Transaction]()
      enum AccountType {
-         case personal
-         case cach
-         case online
+         case normal
+         case VIP
+     }
+     var id: Int
+     var name : String, password: String
+     var amount : Double
+     var type : AccountType
+     
+     init(id: Int, name: String, amount: Double){ // id should be uniqe
+         self.id = id
+         self.name = name
+         self.password = "Password"
+         self.amount = amount
+         self.type = .normal
+     }
+     func getAccount(){ // display account name and amount
+         print("Account name: \(name), Amount: \(amount)")
      }
      
-    
-    init(name: String, amount: Double){
-        self.name = name
-        self.amount = amount
-        self.date = Date()
-    }
-     func getAccount(){
-         print("Account name: \(name), Amount: \(amount), time created this account: \(date)")
+     private mutating func addTransaction(newTrans: Transaction){
+         transactions.append(newTrans)
      }
-    mutating func deposit(money: Double) {
-        amount += money
+     mutating func deposit(transaction: Transaction) { // add money to amount
+        addTransaction(newTrans: transaction)
+         amount += transaction.amount
         print("Total amount in your \(name) account is \(amount)")
     }
-    mutating func withdraw(money: Double){
-        self.amount -= money
+    mutating func withdraw(transaction: Transaction) { // add money to amount
+        // if no money
+        addTransaction(newTrans: transaction)
+         amount -= transaction.amount
         print("Total amount in your \(name) account is \(amount)")
     }
-    
+     //  password: String,type: AccountType
+     func setPassword(password: String) {
+         print(chekPassword(password: password) ? password : "Password Not Valid!!")
+         
+     }
+     func chekPassword(password: String) -> Bool{
+         // regex : Password must be more than 6 characters, with at least one capital, numeric or special character
+         let passwordRegEx = "(?:(?:(?=.*?[0-9])(?=.*?[-!@#$%&*ˆ+=_])|(?:(?=.*?[0-9])|(?=.*?[A-Z])|(?=.*?[-!@#$%&*ˆ+=_])))|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-!@#$%&*ˆ+=_]))[A-Za-z0-9-!@#$%&*ˆ+=_]{6,15}"
+         let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+         return passwordTest.evaluate(with: password)
+     }
 }
 
-var abdullah = Person(name: "Abdullah") // person name
 
-var ahli = Account(name: "Al-Ahli", amount: 20000.79)
-var rajhi = Account(name: "Al-Rajhi", amount: 5000.35)
-var belad = Account(name: "Al-Belad", amount: 200.50)
-abdullah.accounts.append(ahli)
-abdullah.accounts.append(rajhi)
-abdullah.accounts.append(belad)
 
+var abdullah = Person(name: "Abdullah") // create Person instance
+
+var ahli = Account(id: 101, name: "Al-Ahli", amount: 20000.79) //create Account instance
+var rajhi = Account(id: 102, name: "Al-Rajhi", amount: 5000.35)
+var belad = Account(id: 103, name: "Al-Belad", amount: 200.50)
+
+abdullah.addAccount(newAccount: ahli)
+abdullah.addAccount(newAccount: rajhi)
+abdullah.addAccount(newAccount: belad)
 abdullah.getAllAccounts()
 //abdullah.accounts[0].deposit(money: 300) //deposit from Person instance
-rajhi.withdraw(money: 200)
+
+rajhi.setPassword(password: "Abc1228")
+rajhi.withdraw(transaction: Transaction(id: 01, amount: 500, date: Date()))
+
 //abdullah.getAllAmounts()
 
 // remove account
-abdullah.removeAccount(name: "hi")
+abdullah.removeAccount(id: 10)
 //abdullah.getAllAccounts()
+//print(abdullah.accounts.firstIndex(where: {$0.name == "Al-Rajhi"})!)
 
 var ali = Person(name: "Ali")
-var jazira = Account(name: "Al-Jazira", amount: 400)
-jazira.date
-
+var jazira = Account(id: 104, name: "Al-Jazira", amount: 400)
+ali.addAccount(newAccount: jazira)
+//
 ali.getAllAccounts()
 ali.getTotalAmounts()
 
-
-
+/* test transaction */
+abdullah.accounts[0].deposit( transaction: Transaction(id: 1, amount: 99, date: Date()))
 //let findAccount = abdullah.accounts.filter{$0.name == "Al-Rajhi"}
 //findAccount[0].amount
-
