8c6fb6500d91363160fdfbd7524f1d56e7996d91
Abdullah Bajaman <ambajaman@gmail.com>
2021-10-14 06:23:21 +0100

    Update Contents.swift

--- BankAccountManager.playground/Contents.swift
+++ BankAccountManager.playground/Contents.swift
@@ -1,54 +1,5 @@
 import Foundation
 
-
-class Person {
-    
-    var name : String
-    var accounts = [Account]() // array of Account struct
-    
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func getAllAccounts() {
-        if self.accounts.isEmpty { // check accounts array
-            print("Person not exist")
-        }else{
-            self.accounts.map{ account in // loop to display each account in accounts array
-                print("\(name) has account in \(account.name) with Amount of \(account.amount)")
-            }
-            print("Total Amount is: \(getTotalAmounts())")
-        }
-    }
-    
-    func addAccount(newAccount: Account){ // add a new account to accounts array
-        accounts.append(newAccount)
-    }
-    func getTotalAmounts() -> Double {// print total amount
-        let numAccount:Double
-        if accounts.count > 1 {
-            numAccount = accounts.map{$0.amount}.reduce(0, {$0 + $1})
-        } else { numAccount = accounts[0].amount}
-        return numAccount
-    }
-    private func findIndexBy(id: Int) -> Int?{ // return index of accounts array
-        guard let id = accounts.firstIndex(where: {$0.id == id}) else {
-            print("No Account with id: \(id)")
-            return nil
-        }
-        return id
-    }
-    func removeAccount(id: Int){ // remove account by id
-        guard let index = findIndexBy(id: id) else { return  }
-        
-        print("\(accounts[index].name) Account is deleted")
-        accounts.remove(at: index)
-    }
-    
-    
-}
-
 struct Transaction{
     var id: Int, amount: Double, date: Date
     init(id: Int, amount: Double, date: Date = Date()) {
@@ -65,6 +16,11 @@ struct Account {
     var id: Int
     var name : String, password: String
     var amount : Double
+    
+    
+    
+    
+    
     var type : AccountType
     
     init(id: Int, name: String, amount: Double){
@@ -90,6 +46,7 @@ struct Account {
     private mutating func addTransaction(newTrans: Transaction){
         transactions.append(newTrans)
     }
+    
     mutating func deposit(transaction: Transaction) { // add money to amount
         addTransaction(newTrans: transaction)
         amount += transaction.amount
@@ -151,27 +108,130 @@ struct Account {
     
 }
 
-var abdullah = Person(name: "Abdullah") // create Person instance
+
+class User {
+    
+    var name : String
+    var accounts = [Account]() // array of Account struct
+    
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getAllAccounts() {
+        if self.accounts.isEmpty { // check accounts array
+            print("User not exist")
+        }else{
+            self.accounts.map{ account in // loop to display each account in accounts array
+                print("\(name) has account in \(account.name) with Amount of \(account.amount)")
+            }
+            print("Total Amount is: \(getTotalAmounts())")
+        }
+    }
+    
+    func addAccount(newAccount: Account){ // add a new account to accounts array
+        accounts.append(newAccount)
+    }
+    func getTotalAmounts() -> Double {// print total amount
+        let numAccount:Double
+        if accounts.count > 1 {
+            numAccount = accounts.map{$0.amount}.reduce(0, {$0 + $1})
+        } else { numAccount = accounts[0].amount}
+        return numAccount
+    }
+    private func findIndexBy(id: Int) -> Int?{ // return index of accounts array
+        guard let id = accounts.firstIndex(where: {$0.id == id}) else {
+            print("No Account with id: \(id)")
+            return nil
+        }
+        return id
+    }
+    func removeAccount(id: Int){ // remove account by id
+        guard let index = findIndexBy(id: id) else { return  }
+        
+        print("\(accounts[index].name) Account is deleted")
+        accounts.remove(at: index)
+    }
+    func updateAccount(id: Int, account: Account){
+        guard let index = findIndexBy(id: id) else { return  }
+        
+        print("\(accounts[index].name) Account is Updated")
+        accounts[index] = account
+    }
+    
+    
+}
+
+
+var abdullah = User(name: "Abdullah") // create User instance
 /* create Account instance */
 var ahli = Account(id: 101, name: "Al-Ahli", amount: 2000000.79)
 var rajhi = Account(id: 102, name: "Al-Rajhi", amount: 50000.35)
 var belad = Account(id: 103, name: "Al-Belad", amount: 200.50)
 
-/* Add Account to person instance */
+/* Add Account to User instance */
 abdullah.addAccount(newAccount: ahli)
 abdullah.addAccount(newAccount: rajhi)
 abdullah.addAccount(newAccount: belad)
+
+// display all Accounts
 abdullah.getAllAccounts()
 
 /* set and test Password*/
 ahli.getAccount(password: "Password") // default Password
 rajhi.setPassword(password: "Abc1228")
 
+
+
+// check Account Type
+rajhi.type
+ahli.type
+
 /* make transaction and test it */
-var transaction1 = Transaction(id: 01, amount: 10001) // test transaction more than 10000
+var today = Date() // instance from Data struct
+
+var formatter = DateFormatter() // instance from DateFormatter class
+formatter.dateFormat = "dd/MM/yyyy HH:mm"
+
+// variables contain different year, month and day to test sort transactions
+var date1 = Calendar.current.date(byAdding: .year, value: 2, to: today)!
+var date2 = Calendar.current.date(byAdding: .day, value:  -2, to: today)!
+var date3 = Calendar.current.date(byAdding: .day, value:  -3, to: today)!
+var date4 = Calendar.current.date(byAdding: .day, value:  1, to: today)!
+var date5 = Calendar.current.date(byAdding: .month, value: -5, to: today)!
+
+// create transactions instance with default date
+var transaction1 = Transaction(id: 01, amount: 10001) // test withdraw more than 10000
 var transaction2 = Transaction(id: 02, amount: 500)
 var transaction3 = Transaction(id: 03, amount: 6000)
+// create transactions with costum date
+var transaction4 = Transaction(id: 04, amount: 8000, date: date2)
+var transaction5 = Transaction(id: 05, amount: 700, date: date1)
+var transaction6 = Transaction(id: 06, amount: 6000, date: date5)
 
+// use transactions
+rajhi.withdraw(transaction: Transaction(id: 04, amount: 200), password: "Abc1228")
+rajhi.withdraw(transaction: transaction1, password: "Abc1228")
+rajhi.withdraw(transaction: transaction2, password: "Abc1228")
+rajhi.withdraw(transaction: transaction3, password: "Abc1228")
+
+ahli.deposit(transaction: transaction4)
+ahli.deposit(transaction: transaction5)
+ahli.deposit(transaction: transaction6)
+
+ahli.withdraw(transaction: transaction1, password: "Password")
+ahli.withdraw(transaction: transaction2, password: "Password")
+
+
+// remove account
+abdullah.removeAccount(id: 10) // not existed id
+abdullah.removeAccount(id: 102) //
+abdullah.getAllAccounts() // check all accounts
+
+// update account
+abdullah.updateAccount(id: 103, account: Account(id: 103, name: "Lloyds", amount: 500))
+abdullah.getAllAccounts()
 /*  Add extension to Account to sort transactions */
 extension Account {
     func sortedTransaction(){
@@ -185,49 +245,4 @@ extension Account {
         }
     }
 }
-
-/* test sorted transaction */
-var today = Date()
-
-var formatter = DateFormatter()
-formatter.dateFormat = "dd/MM/yyyy HH:mm"
-formatter.timeZone = .current
-
-var date1 = Calendar.current.date(byAdding: .year, value: 2, to: today)!
-var date2 = Calendar.current.date(byAdding: .day, value:  -2, to: today)!
-var date3 = Calendar.current.date(byAdding: .day, value:  -3, to: today)!
-var date4 = Calendar.current.date(byAdding: .day, value:  1, to: today)!
-var date5 = Calendar.current.date(byAdding: .month, value: -5, to: today)!
-
-
-var transaction4 = Transaction(id: 04, amount: 6000, date: date2)
-var transaction5 = Transaction(id: 05, amount: 6000, date: date1)
-var transaction6 = Transaction(id: 06, amount: 6000, date: date5)
-
-
-rajhi.withdraw(transaction: Transaction(id: 04, amount: 200), password: "Abc1228")
-rajhi.withdraw(transaction: transaction1, password: "Abc1228")
-rajhi.withdraw(transaction: transaction2, password: "Abc1228")
-rajhi.withdraw(transaction: transaction3, password: "Abc1228")
-
-ahli.deposit(transaction: transaction4)
-
-ahli.deposit(transaction: transaction5)
-
-ahli.deposit(transaction: transaction6)
-
-ahli.withdraw(transaction: transaction1, password: "Password")
-
-ahli.withdraw(transaction: transaction2, password: "Password")
-
-// check Account Type
-rajhi.type
-ahli.type
-
-// remove account
-abdullah.removeAccount(id: 10) // not existed id
-abdullah.removeAccount(id: 103)
-abdullah.getAllAccounts() // check all accounts
-
-
 ahli.sortedTransaction()
